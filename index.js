12b6d3efc6fff65763487530c1c4950bc7ee45b2
Jeff Brown <jeff.brown@gmail.com>
2022-03-07 23:37:07 -0800

    Add Zwave2MQTT transport, query node information.
    Checks for compatible firmware and prompts the user.
    Send and receive functions are stubbed out.

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const fs = require('fs');
 const crypto = require('crypto');
 const readline = require('readline');
 const { once } = require('events');
+const mqtt = require('mqtt');
 
 // The Intermatic manufacturer ID for ZWave products.
 const manufacturerId = 0x0005;
+const productType = 0x5045; // 'PE'
+
+function getFirmwareLabel(nodeInfo) {
+    if (nodeInfo.manufacturerId === manufacturerId &&
+            nodeInfo.productType === productType) {
+        if (nodeInfo.productId === 0x0653) return "PE0653";
+        if (nodeInfo.productId === 0x0953) return "PE0953";
+    }
+    return null;
+}
 
 function sha256(blob) {
     let hash = crypto.createHash('sha256');
@@ -180,16 +191,16 @@ function updateCRC16(crc16, byte) {
 
 class LogTransport {
     constructor(inner) {
-        this.inner = inner;
+        this._inner = inner;
     }
 
     async send(packet) {
         console.log(`>> SEND ${packet}`);
-        return await this.inner.send(packet);
+        return await this._inner.send(packet);
     }
 
     async receive() {
-        const packet = await this.inner.receive();
+        const packet = await this._inner.receive();
         console.log(`<< RECV ${packet}`);
         return packet;
     }
@@ -197,10 +208,10 @@ class LogTransport {
 
 class FakeTransport {
     constructor(debug) {
-        this.pendingReply = null;
-        this.nextSeq = 0;
-        this.state = 'wait';
-        this.debug = debug;
+        this._pendingReply = null;
+        this._nextSeq = 0;
+        this._state = 'wait';
+        this._debug = debug;
     }
 
     async send(packet) {
@@ -210,53 +221,53 @@ class FakeTransport {
         const seq = packet.length >= 4 ? packet[2] | (packet[3] << 8) : -1;
         switch (type) {
             case packetStart: {
-                if (this.state !== 'wait') return;
+                if (this._state !== 'wait') return;
 
-                if (this.debug)
+                if (this._debug)
                     console.log('!! START');
-                this.state = 'transfer';
-                this.nextSeq = 0;
+                this._state = 'transfer';
+                this._nextSeq = 0;
                 break;
             }
             case packetData: {
-                if (this.state !== 'transfer' || this.nextSeq !== seq || packet.length < 6) return;
+                if (this._state !== 'transfer' || this._nextSeq !== seq || packet.length < 6) return;
 
                 const data = packet.slice(4, packet.length - 2);
                 const crc = packet[packet.length - 2] | (packet[packet.length - 1] << 8);
                 let check = 0;
                 for (let i = 0; i < packet.length - 2; i++)
                     check = updateCRC16(check, packet[i]);
-                if (this.debug)
+                if (this._debug)
                     console.log(`!! DATA ${seq} ${data} ${crc} (${check})`);
 
                 if (check !== crc) {
-                    this.state = 'error';
+                    this._state = 'error';
                 } else {
-                    this.nextSeq++;
+                    this._nextSeq++;
                 }
                 break;
             }
             case packetDone: {
-                if (this.state !== 'transfer' || this.nextSeq !== seq || packet.length < 4) return;
+                if (this._state !== 'transfer' || this._nextSeq !== seq || packet.length < 4) return;
 
-                if (this.debug)
+                if (this._debug)
                     console.log(`!! DONE ${seq}`);
-                this.state = 'done';
+                this._state = 'done';
                 break;
             }
         }
-        switch (this.state) {
+        switch (this._state) {
             case 'transfer':
-                this.pendingReply = [commandFirmwareTransfer, packetDataRequest,
-                        this.nextSeq & 255, this.nextSeq >> 8];
+                this._pendingReply = [commandFirmwareTransfer, packetDataRequest,
+                        this._nextSeq & 255, this._nextSeq >> 8];
                 break;
             case 'error':
-                this.pendingReply = [commandFirmwareTransfer, packetCRCError,
-                        this.nextSeq & 255, this.nextSeq >> 8];
+                this._pendingReply = [commandFirmwareTransfer, packetCRCError,
+                        this._nextSeq & 255, this._nextSeq >> 8];
                 break;
             case 'done':
-                this.pendingReply = [commandFirmwareTransfer, packetDone,
-                        this.nextSeq & 255, this.nextSeq >> 8];
+                this._pendingReply = [commandFirmwareTransfer, packetDone,
+                        this._nextSeq & 255, this._nextSeq >> 8];
                 break;
         }
     }
@@ -264,9 +275,102 @@ class FakeTransport {
     receive() {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
-                resolve(this.pendingReply);
-                this.pendingReply = null;
+                resolve(this._pendingReply);
+                this._pendingReply = null;
             }, 5);
+        });
+    }
+}
+
+class ZwaveJS2MqttTransport {
+    constructor(url, api, debug) {
+        this._url = url;
+        this._callTopic = api + '/driverFunction/set';
+        this._resultTopic = api + '/driverFunction';
+        this._resolveResult = null;
+        this._debug = debug;
+    }
+
+    async connect() {
+        this._client = mqtt.connect(this._url);
+
+        this._client.on('close', () => {
+            console.log('Disconnected from MQTT');
+        });
+
+        this._client.on('error', (error) => {
+            console.log(`MQTT error: ${error}`);
+        });
+
+        this._client.on('message', (topic, message) => {
+            if (topic === this._resultTopic && this._resolveResult) {
+                this._resolveResult(JSON.parse(message.toString()));
+                this._resolveResult = null;
+            }
+        });
+
+        await once(this._client, 'connect');
+
+        await this._subscribe(this._resultTopic);
+        console.log('Connected to Zwave2MQTT server via MQTT');
+    }
+
+    getNodeInfo(nodeId) {
+        return this._driverFunction(`
+            const nodeId = ${nodeId};
+            const node = driver.controller.nodes.get(nodeId);
+            if (node === undefined) return null;
+            return {
+                nodeId: node.id,
+                name: node.name,
+                location: node.location,
+                firmwareVersion: node.firmwareVersion,
+                manufacturerId: node.manufacturerId,
+                productId: node.productId,
+                productType: node.productType
+            };
+        `);
+    }
+
+    async send(packet) {
+        // call driver.sendCommand?
+    }
+
+    async receive() {
+        // call driver.waitForCommand?
+        return null;
+    }
+
+    async _driverFunction(code) {
+        const call = JSON.stringify({ args: [ code ]});
+        await this._publish(this._callTopic, call);
+
+        const response = await new Promise((resolve, reject) => {
+            this._resolveResult = resolve;
+        });
+        if (this._debug)
+            console.dir(response);
+
+        if (response.args[0] !== code)
+            throw Error('Driver function call response mismatch');
+        if (!response.success)
+            throw Error(`Driver function call failed: ${response.message}`);
+        return response.result;
+    }
+
+    _publish (...args) {
+        return new Promise((resolve, reject) => {
+            this._client.publish(...args, (err, result) => {
+                if (err) reject(err); else resolve(result);
+            });
+        });
+    }
+
+    _subscribe (...args) {
+        return new Promise((resolve, reject) => {
+            this._client.subscribe(...args, (err, result) => {
+                if (err) reject(err); else resolve(result);
+            });
         });
     }
 }
@@ -342,17 +446,94 @@ program.command('describe')
     .argument('<file>', 'path to firmware archive (*.iboot)')
     .action(async (file, options) => {
         const archive = await readFirmwareArchive(file);
+
+        console.log('Firmware images:');
         console.dir(archive);
     });
 
 program.command('fake-upload')
-    .description('Pretends to upload a firmware archive')
+    .description('Pretends to upload firmware to a device')
     .argument('<file>', 'path to firmware archive (*.iboot)')
     .option('-d', 'debug output')
     .action(async (file, options) => {
         const archive = await readFirmwareArchive(file);
-        console.dir(archive);
+        if (options.d) {
+            console.log('Firmware images:');
+            console.dir(archive);
+        }
+
         let transport = new FakeTransport(options.d);
+        if (options.d) transport = new LogTransport(transport);
+
+        const result = await uploadFirmware(archive.products['PE0653'].blob, transport);
+        if (!result) process.exit(1);
+    });
+
+program.command('upload')
+    .description('Uploads firmware to a device')
+    .argument('<file>', 'path to firmware archive (*.iboot)')
+    .argument('<nodeId>', 'Zwave node id to update')
+    .argument('<mqtt>', 'ZwaveJS2MQTT server\'s MQTT broker URL, e.g. mqtt://user:password@host/')
+    .argument('<api>', 'ZwaveJS2MQTT server\'s API topic, e.g. zwavejs/_CLIENTS/ZWAVE_GATEWAY-HomeAssistant/api')
+    .option('-d', 'debug output')
+    .action(async (file, nodeId, mqtt, api, options) => {
+        const archive = await readFirmwareArchive(file);
+        if (options.d) {
+            console.log('Firmware images:');
+            console.dir(archive);
+        }
+
+        let transport = new ZwaveJS2MqttTransport(mqtt, api, options.d);
+        await transport.connect();
+
+        const nodeInfo = await transport.getNodeInfo(nodeId);
+        if (nodeInfo === null) {
+            console.error(`Could not get information about node ${nodeId}`);
+            process.exit(1);
+        }
+        if (options.d) {
+            console.log('Node information:');
+            console.dir(nodeInfo);
+        }
+
+        console.log('');
+        console.log(`Node to upgrade:`);
+        console.log(`- nodeId: ${nodeInfo.id}`);
+        console.log(`- name: ${nodeInfo.name}`);
+        console.log(`- location: ${nodeInfo.location}`);
+        console.log(`- current firmware version: ${nodeInfo.firmwareVersion}`);
+        console.log('');
+
+        const firmwareLabel = getFirmwareLabel(nodeInfo);
+        if (firmwareLabel === null) {
+            console.error(`This program does not support upgrading the firmware of this node`);
+            process.exit(1);
+        }
+
+        const product = archive.products[firmwareLabel];
+        if (product === undefined) {
+            console.error(`The provided firmware archive does not contain a blob for product ${firmwareLabel}`);
+            process.exit(1);
+        }
+
+        console.log(`Upgrade to perform:`);
+        console.log(`- new firmware version: ${product.version}`);
+        console.log(`- new firmware hash: ${product.blobHash})`);
+        console.log(`- product id: ${firmwareLabel}`);
+        console.log(`- product name: ${product.name}`);
+        console.log(`- product notice: ${product.message}`);
+        console.log('');
+
+        const reader = readline.createInterface({ input: process.stdin, output: process.stdout });
+        const answer = await new Promise((resolve, reject) => {
+            reader.question('Proceed? [Enter "YES" to confirm] ', resolve);
+        });
+        reader.close();
+        if (answer !== 'YES') {
+            console.error('Upgrade declined by user');
+            process.exit(1);
+        }
+
         if (options.d) transport = new LogTransport(transport);
         const result = await uploadFirmware(archive.products['PE0653'].blob, transport);
         if (!result) process.exit(1);
